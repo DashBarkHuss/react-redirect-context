66bf20e847b936557ebd952b01753d1cbb5079d5
Dashiell BarkHuss <dashiellbarkhuss@gmail.com>
2021-02-25 21:53:55 -0500

    no solution

--- src/PayForAccess.js
+++ src/PayForAccess.js
@@ -1,36 +1,23 @@
-import React, { useContext, useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import { UserContext } from './contexts/UserContext';
+import React, { useContext, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { UserContext } from "./contexts/UserContext";
 
 export default function PayForAccess() {
   const [next, setNext] = useState(false);
-  // const [user, setUser] = useState(false);
-
-  const getCurrentUser = async () => {
-    const user = await fetch('/users/current', {}).then(async (res) => {
-      const j = await res.json();
-      return j;
-    });
-    return user;
-  };
 
   const currentUser = useContext(UserContext);
 
   return (
     <div>
-      Logged In:{' '}
-      {!currentUser || currentUser.message === 'not logged in' ? 'No One' : currentUser.username}{' '}
+      Logged In:{" "}
+      {!currentUser || currentUser.message === "not logged in"
+        ? "No One"
+        : currentUser.username}{" "}
       <br></br>
       <button
         onClick={() => {
-          fetch('/pay', { method: 'POST' }).then((res) => {
-            // if (res.status === 201) {
-            //   getCurrentUser().then((user) => {
-            //     setUser(user);
-            //     setNext(true);
-            //   });
-            // }
-            setNext(true);
+          fetch("/pay", { method: "POST" }).then((res) => {
+            if (res.status === 201) setNext(true);
           });
         }}
       >
